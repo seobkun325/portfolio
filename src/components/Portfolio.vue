16950f40775717eb044c15a35af26c9e4499e05a
seobkun325 <seobkun325@gmail.com>
2024-09-24 23:52:44 +0900

    첫 커밋 완료

---
+++ src/components/Portfolio.vue
@@ -0,0 +1,20 @@
+<template>
+  <button @click="count">{{ num }}</button>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+
+const num = ref(0)
+  const count = () => {
+  num.value++
+}
+</script>
+
+<style>
+button {
+  width: 30px;
+  height: 30px;
+}
+</style>
+
